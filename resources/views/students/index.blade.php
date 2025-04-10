94c0752f31ff12f08ff2ebb22846be640424969d
ashikurdev <ashikgsc170@gmail.com>
2025-04-10 08:53:04 +0600

    form

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -278,7 +278,7 @@
                                         data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">Actions
                                         <i class="ki-outline ki-down fs-5 m-0"></i></a>
                                     <!--begin::Menu-->
-                                    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-125px py-4"
+                                    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-150px py-4"
                                         data-kt-menu="true">
                                         <!--begin::Menu item-->
                                         <div class="menu-item px-3">
@@ -286,7 +286,7 @@
                                         </div>
                                         <!--begin::Menu item-->
                                         <div class="menu-item px-3">
-                                            <a href="#" class="menu-link px-3">Inactive</a>
+                                            <a href="#" class="menu-link px-3">Active/Inactive</a>
                                         </div>
                                         <!--end::Menu item-->
                                         <!--begin::Menu item-->
