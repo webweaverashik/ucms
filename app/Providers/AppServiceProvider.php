d02777fb69598c26be0a72562f28ad5e3b817c15
ashikurdev <ashikgsc170@gmail.com>
2025-05-24 22:43:10 +0600

    provider updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,11 +1,11 @@
 <?php
+
 namespace App\Providers;
 
 use App\Models\Student\Student;
 use App\Models\Teacher\Teacher;
-use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
-use Spatie\Browsershot\Browsershot;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,10 +26,5 @@ class AppServiceProvider extends ServiceProvider
             'student' => Student::class,
             'teacher' => Teacher::class,
         ]);
-
-        Browsershot::create()
-            ->setNodeBinary('/home/uniqueco/.nvm/versions/node/v22.16.0/bin/node')
-            ->setNpmBinary('/home/uniqueco/.nvm/versions/node/v22.16.0/bin/npm')
-            ->setOption('args', ['--no-sandbox']);
     }
 }
