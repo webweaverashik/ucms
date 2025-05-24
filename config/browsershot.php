ae38f1a5bd8a198b57a487f6188f9344fa990093
ashikurdev <ashikgsc170@gmail.com>
2025-05-24 23:22:25 +0600

    pdf download issue

---
+++ config/browsershot.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'remote_instance_url' => 'https://chrome.browserless.io/pdf?token=2SN5w1ISBOIQFjK39c9539b9bf2f5e4de607ab78dd5d9d5e8',
+    'options'             => [
+        'args'    => [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+        ],
+        'timeout' => 60,
+    ],
+];
