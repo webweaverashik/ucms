8673faed75cdf6283cea77047e3658f86d851d01
ashikurdev <ashikgsc170@gmail.com>
2025-05-24 22:51:18 +0600

    pdf download issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,11 +1,11 @@
 <?php
+
 namespace App\Providers;
 
 use App\Models\Student\Student;
 use App\Models\Teacher\Teacher;
-use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
-use Spatie\Browsershot\Browsershot;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,11 +26,5 @@ class AppServiceProvider extends ServiceProvider
             'student' => Student::class,
             'teacher' => Teacher::class,
         ]);
-
-        Browsershot::setNodeBinary('/home/uniqueco/.nvm/versions/node/v22.16.0/bin/node');
-        Browsershot::setNpmBinary('/home/uniqueco/.nvm/versions/node/v22.16.0/bin/npm');
-
-        // Optional for shared hosting
-        Browsershot::setOption('args', ['--no-sandbox']);
     }
 }
