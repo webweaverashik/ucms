fc2f0c2d1d416aa4af859bde78368f8ca9c1f5db
ashikurdev <ashikgsc170@gmail.com>
2025-06-02 14:18:12 +0600

    invoice schedule enabled

--- routes/console.php
+++ routes/console.php
@@ -11,5 +11,5 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('invoices:generate-monthly')
-    ->everyFiveMinutes() // Run on 1st day of month at 12:30 AM
+    ->monthlyOn(1, '00:30') // Run on 1st day of month at 12:30 AM
     ->appendOutputTo(storage_path('logs/invoice-generation.log'));
