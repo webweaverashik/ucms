6e9b5c8d7bdf818751adbec5c857260e4dbc8bfc
ashikurdev <ashikgsc170@gmail.com>
2025-06-20 23:06:14 +0600

    ok

--- resources/views/layouts/partials/scripts.blade.php
+++ resources/views/layouts/partials/scripts.blade.php
@@ -27,7 +27,7 @@
                 "onclick": null,
                 "showDuration": "300",
                 "hideDuration": "1000",
-                "timeOut": "3000",
+                "timeOut": "2000",
                 "extendedTimeOut": "1000",
                 "showEasing": "swing",
                 "hideEasing": "linear",
