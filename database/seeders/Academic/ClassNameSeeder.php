f98a7028059740c2c0bc4affb7cf373d02b1efa3
ashikurdev <ashikgsc170@gmail.com>
2025-07-30 22:39:03 +0600

    class added

--- database/seeders/Academic/ClassNameSeeder.php
+++ database/seeders/Academic/ClassNameSeeder.php
@@ -9,15 +9,18 @@ class ClassNameSeeder extends Seeder
     public function run(): void
     {
         $classes = [
-            'Class IV'   => '04',
-            'Class V'    => '05',
-            'Class VI'   => '06',
-            'Class VII'  => '07',
-            'Class VIII' => '08',
-            'Class IX'   => '09',
-            'Class X'    => '10',
-            'Class XI'   => '11',
-            'Class XII'  => '12',
+            'Class IV'        => '04',
+            'Class V'         => '05',
+            'Class VI'        => '06',
+            'Class VII'       => '07',
+            'Class VIII'      => '08',
+            'Class IX'        => '09',
+            'SSC (25-26)'     => '10',
+            'SSC (24-25)'     => '10',
+            'HSC (25-26) Sci' => '11',
+            'HSC (25-26) Com' => '11',
+            'HSC (24-25) Sci' => '12',
+            'HSC (24-25) Com' => '12',
         ];
 
         foreach ($classes as $name => $numeral) {
