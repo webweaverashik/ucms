fc96de1ec4d6440427f4294d4b5de766f5976e0d
ashikurdev <ashikgsc170@gmail.com>
2025-07-31 21:55:33 +0600

    students table indexed

---
+++ database/migrations/2025_07_31_214828_add_indexes_to_students_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->index('branch_id');
+            $table->index('class_id');
+            $table->index('shift_id');
+            $table->index('academic_group');
+            $table->index('deleted_at');
+            $table->index(['class_id', 'academic_group', 'shift_id'], 'students_class_group_shift_index'); // custom name to avoid default name collision
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('students', function (Blueprint $table) {
+            Schema::table('students', function (Blueprint $table) {
+                $table->dropIndex(['branch_id']);
+                $table->dropIndex(['class_id']);
+                $table->dropIndex(['shift_id']);
+                $table->dropIndex(['academic_group']);
+                $table->dropIndex(['deleted_at']);
+                $table->dropIndex('students_class_group_shift_index');
+            });
+        });
+    }
+};
