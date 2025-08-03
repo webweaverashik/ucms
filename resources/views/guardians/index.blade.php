d5b059804ffe9f04bc3d5822c92617bc0f03ce32
ashikurdev <ashikgsc170@gmail.com>
2025-08-03 23:37:54 +0600

    mobile layout fixed

--- resources/views/guardians/index.blade.php
+++ resources/views/guardians/index.blade.php
@@ -64,7 +64,7 @@
                 <!--begin::Search-->
                 <div class="d-flex align-items-center position-relative my-1">
                     <i class="ki-outline ki-magnifier fs-3 position-absolute ms-5"></i> <input type="text"
-                        data-kt-subscription-table-filter="search" class="form-control form-control-solid w-200px w-sm-400px ps-12"
+                        data-kt-subscription-table-filter="search" class="form-control form-control-solid w-250px w-sm-400px ps-12"
                         placeholder="Search In Guardians">
                 </div>
                 <!--end::Search-->
