cc7de07b7b691f0a814a84d34535a3d6840f0b33
ashikurdev <ashikgsc170@gmail.com>
2025-08-07 13:11:29 +0600

    page speed up

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -10,7 +10,7 @@
         <!--end::Copyright-->
         <!--begin::Menu-->
         <ul class="menu menu-gray-600 menu-hover-primary fw-semibold order-1">
-            <li class="menu-item"><a href="#" target="_blank" class="menu-link px-2">About</a></li>
+            <li class="menu-item"><a href="#" target="_blank" class="menu-link px-2">v1.9.1</a></li>
             <li class="menu-item"><a href="#" target="_blank"
                     class="menu-link px-2">Support</a></li>
         </ul>
