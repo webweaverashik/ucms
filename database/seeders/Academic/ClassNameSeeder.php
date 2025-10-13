27e4ed106eeddb1e08d1cbee8d7c2a2e77998f09
ashikurdev <ashikgsc170@gmail.com>
2025-10-13 21:13:53 +0600

    ok

--- database/seeders/Academic/ClassNameSeeder.php
+++ database/seeders/Academic/ClassNameSeeder.php
@@ -9,27 +9,33 @@ class ClassNameSeeder extends Seeder
     public function run(): void
     {
         $classes = [
-            'Class IV'        => '04',
-            'Class V'         => '05',
-            'Class VI'        => '06',
-            'Class VII'       => '07',
-            'Class VIII'      => '08',
-            'Class IX'        => '09',
-            'SSC (25-26)'     => '10',
-            'SSC (24-25)'     => '10',
-            'HSC (26-27) Sci' => '11',
-            'HSC (26-27) Com' => '11',
-            'HSC (25-26) Sci' => '12',
-            'HSC (25-26) Com' => '12',
-            'HSC (24-25) Sci' => '12',
-            'HSC (24-25) Com' => '12',
+            ['name' => 'Class IV',        'class_numeral' => '04', 'is_active' => 1],
+            ['name' => 'Class V',         'class_numeral' => '05', 'is_active' => 1],
+            ['name' => 'Class VI',        'class_numeral' => '06', 'is_active' => 1],
+            ['name' => 'Class VII',       'class_numeral' => '07', 'is_active' => 1],
+            ['name' => 'Class VIII',      'class_numeral' => '08', 'is_active' => 1],
+            ['name' => 'Class IX',        'class_numeral' => '09', 'is_active' => 1],
+            ['name' => 'SSC (25-26)',     'class_numeral' => '10', 'is_active' => 1],
+            ['name' => 'SSC (24-25)',     'class_numeral' => '10', 'is_active' => 0],
+            ['name' => 'HSC (26-27) Sci', 'class_numeral' => '11', 'is_active' => 1],
+            ['name' => 'HSC (26-27) Com', 'class_numeral' => '11', 'is_active' => 1],
+            ['name' => 'HSC (25-26) Sci', 'class_numeral' => '12', 'is_active' => 1],
+            ['name' => 'HSC (25-26) Com', 'class_numeral' => '12', 'is_active' => 1],
+            ['name' => 'HSC (24-25) Sci', 'class_numeral' => '12', 'is_active' => 0],
+            ['name' => 'HSC (24-25) Com', 'class_numeral' => '12', 'is_active' => 0],
         ];
 
-        foreach ($classes as $name => $numeral) {
-            ClassName::firstOrCreate([
-                'name'          => $name,
-                'class_numeral' => $numeral,
-            ]);
+        foreach ($classes as $class) {
+            ClassName::updateOrCreate(
+                [
+                    'name'          => $class['name'],
+                    'class_numeral' => $class['class_numeral'],
+                ],
+                [
+                    'is_active'     => $class['is_active'],
+                ]
+            );
         }
     }
 }
+
