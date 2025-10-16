5f11b8669afd2ee4c90a79473b5a2f9a55e36dbd
ashikurdev <ashikgsc170@gmail.com>
2025-10-16 11:19:20 +0600

    statement layout done 3

--- resources/views/pdf/student_statement.blade.php
+++ resources/views/pdf/student_statement.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $student->name }}- টাকা প্রদানের রশিদ</title>
+    <title>{{ $student->name }} - টাকা প্রদানের রশিদ</title>
 
     <link href="https://fonts.maateen.me/solaiman-lipi/font.css" rel="stylesheet">
 
