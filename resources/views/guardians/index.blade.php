720686efcb1dcffa083d53544341e6e7ad95349c
ashikurdev <ashikgsc170@gmail.com>
2026-01-10 10:20:27 +0600

    updated

--- resources/views/guardians/index.blade.php
+++ resources/views/guardians/index.blade.php
@@ -66,7 +66,7 @@
                     <div class="d-flex align-items-center position-relative my-1">
                         <i class="ki-outline ki-magnifier fs-3 position-absolute ms-5"></i> <input type="text"
                             data-kt-subscription-table-filter="search"
-                            class="form-control form-control-solid w-350px ps-12"
+                            class="form-control form-control-solid w-md-350px ps-12"
                             placeholder="Search In Guardians">
                     </div>
                     <!--end::Search-->
