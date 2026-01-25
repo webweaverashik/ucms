5b72635d830de9caf1e3a12a8040c4e0c961f7bc
ashikurdev <ashikgsc170@gmail.com>
2026-01-25 13:21:38 +0600

    AJAXified

--- resources/views/guardians/partials/guardians-table.blade.php
+++ resources/views/guardians/partials/guardians-table.blade.php
@@ -2,7 +2,7 @@
 <table class="table table-hover align-middle table-row-dashed fs-6 gy-5 ucms-table" id="{{ $tableId }}">
     <thead>
         <tr class="fw-bold fs-7 text-uppercase gs-0">
-            <th class="w-25px">SL</th>
+            <th class="w-50px">SL</th>
             <th>Name</th>
             <th>Mobile</th>
             <th>Gender</th>
