de67ec14e487ada35313e61b14d9f8d73db718bd
ashikurdev <ashikgsc170@gmail.com>
2026-05-18 23:44:26 +0600

    Old Backup Auto Delete Feature Added

--- config/backup.php
+++ config/backup.php
@@ -297,33 +297,33 @@ return [
              * of that day will be kept. Older backups within the same day will be removed.
              * If you create backups only once a day, no backups will be removed yet.
              */
-            'keep_daily_backups_for_days'                          => 16,
+            'keep_daily_backups_for_days'                          => 7,
 
             /*
              * After the "keep_daily_backups_for_days" period is over, the most recent backup
              * of that week will be kept. Older backups within the same week will be removed.
              * If you create backups only once a week, no backups will be removed yet.
              */
-            'keep_weekly_backups_for_weeks'                        => 8,
+            'keep_weekly_backups_for_weeks'                        => 0,
 
             /*
              * After the "keep_weekly_backups_for_weeks" period is over, the most recent backup
              * of that month will be kept. Older backups within the same month will be removed.
              */
-            'keep_monthly_backups_for_months'                      => 4,
+            'keep_monthly_backups_for_months'                      => 0,
 
             /*
              * After the "keep_monthly_backups_for_months" period is over, the most recent backup
              * of that year will be kept. Older backups within the same year will be removed.
              */
-            'keep_yearly_backups_for_years'                        => 2,
+            'keep_yearly_backups_for_years'                        => 0,
 
             /*
              * After cleaning up the backups remove the oldest backup until
              * this amount of megabytes has been reached.
              * Set null for unlimited size.
              */
-            'delete_oldest_backups_when_using_more_megabytes_than' => 5000,
+            'delete_oldest_backups_when_using_more_megabytes_than' => 500,
         ],
 
         /*
