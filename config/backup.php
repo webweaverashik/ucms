e6f01ecde3e2aca57be6c81aa365a9d782ef8810
ashikurdev <ashikgsc170@gmail.com>
2026-05-18 23:46:18 +0600

    Old Backup Auto Delete Feature Added

--- config/backup.php
+++ config/backup.php
@@ -297,7 +297,7 @@ return [
              * of that day will be kept. Older backups within the same day will be removed.
              * If you create backups only once a day, no backups will be removed yet.
              */
-            'keep_daily_backups_for_days'                          => 7,
+            'keep_daily_backups_for_days'                          => 0,
 
             /*
              * After the "keep_daily_backups_for_days" period is over, the most recent backup
